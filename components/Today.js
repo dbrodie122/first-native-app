558faa526c2681c95caf60567877be11e5919ef1
David Brodie <david.brodie122@gmail.com>
2017-11-23 13:12:48 -0500

    Add component

---
+++ components/Today.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Today extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>The Today page.</Text>
+      </View>
+    );
+  }
+};
+
+export default Today;
